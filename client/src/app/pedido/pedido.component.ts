976dc2b6d414001af6629f4be7ce9fc2eb66de9f
Maria Cecília Coelho Segati <109155054+MariaCCSegati@users.noreply.github.com>
2022-12-08 16:56:55 -0300

    commit final 08-12
    tentativa de fazer o autocomplete --> spoiler: não consegui

--- client/src/app/pedido/pedido.component.ts
+++ client/src/app/pedido/pedido.component.ts
@@ -1,11 +1,29 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { MatDialog } from '@angular/material/dialog';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { map, Observable, startWith } from 'rxjs';
 import { HttpService } from 'src/services/http.service';
 import { ModalPedidoComponent } from '../modal-pedido/modal-pedido.component';
-
+export interface DialogDataClient{
+  logradouro: string;
+  clientes : Array<any>;
+  address: any[];
+  id: number,
+  idEndereco: number,
+  nome: string,
+  CNPJ: string, 
+  razaoSocial: string, 
+  clienteDesde: string,
+  rua: string,
+  bairro:string,
+  cidade:string,
+  estado:string,
+  cep:string,
+  numero:string,
+  complemento:string,
+  referencia:string
+}
 @Component({
   selector: 'app-pedido',
   templateUrl: './pedido.component.html',
@@ -15,16 +33,21 @@ export class PedidoComponent implements OnInit {
 
   selectedEndereco: number | undefined;
   public enderecos : Array<any> = [];
+  public clientes : Array<any> = [];
   nome = '';
   pedidos: Array<any> = [];
 
-  constructor(private http : HttpClient, private httpService : HttpService, public dialog: MatDialog) { }
+  constructor(private http : HttpClient, private httpService : HttpService, public dialog: MatDialog, @Inject(MAT_DIALOG_DATA) public data: DialogDataClient) { }
 
   myControl = new FormControl('');
-  options: string[] = [];
+  options: string[] = this.clientes;
   filteredOptions: Observable<string[]> | undefined;
 
-  ngOnInit() {
+  async ngOnInit() {
+    await this.filtro()
+  }
+
+  filtro(){
     this.filteredOptions = this.myControl.valueChanges.pipe(
       startWith(''),
       map(value => this._filter(value || '')),
@@ -38,7 +61,11 @@ export class PedidoComponent implements OnInit {
   }
 
   async getEndereco(){
-    this.enderecos = await this.httpService.get('collection');
+    this.enderecos = await this.httpService.get('client');
+  }
+
+  async getClientes(){
+    this.clientes = await this.httpService.get('client');
   }
 
   openDialog(){
